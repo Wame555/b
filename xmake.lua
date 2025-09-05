3e6366562c0fec687184c25934c791fa76708556
Wame555 <76699366+Wame555@users.noreply.github.com>
2025-09-05 21:05:39 +0200

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -7,8 +7,10 @@ set_xmakever("2.7.3")
 set_languages("cxx17")
 add_rules("mode.debug", "mode.release")
 
+-- Qt6 útvonala manuális beállítása
+set_env("QT6_DIR", "C:\Qt\6.9.2\msvc2022_64")  -- Cseréld le a saját Qt6 útvonaldra!
+
 -- Csomagok hozzáadása (automatikus letöltés)
-add_requires("qt6", {system = false})
 add_requires("ixwebsocket", {system = false})
 add_requires("nlohmann_json", {system = false})
 add_requires("libcurl", {system = false})
@@ -18,5 +20,7 @@ target("CryptoBot")
     set_kind("binary")
     add_files("src/*.cpp")
     add_includedirs("include", {public = true})
-    add_packages("qt6", "ixwebsocket", "nlohmann_json", "libcurl")
+    add_packages("ixwebsocket", "nlohmann_json", "libcurl")
     add_links("Qt6Widgets", "Qt6Network")
+    add_linkdirs("$env(QT6_DIR)/lib")
+    add_includedirs("$env(QT6_DIR)/include")
