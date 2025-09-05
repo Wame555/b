d6e3ed44fc05dcc9d9a4814a4f9251be22e5c1e8
Wame555 <76699366+Wame555@users.noreply.github.com>
2025-09-05 20:59:00 +0200

    Create binancewebsocket.h

---
+++ include/binancewebsocket.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <QObject>
+#include <ixwebsocket/IXWebSocket.h>
+
+class BinanceWebsocket : public QObject {
+    Q_OBJECT
+public:
+    explicit BinanceWebsocket(QObject* parent = nullptr);
+    void connectToBinance(const QStringList& symbols);
+
+signals:
+    void priceUpdated(const QString& symbol, double price);
+
+private:
+    ix::WebSocket webSocket;
+};
