77fde91098bc5a461e92689e7448c2ba90109f5c
Wame555 <76699366+Wame555@users.noreply.github.com>
2025-09-05 20:54:42 +0200

    Create xmake.lua

---
+++ xmake.lua
@@ -0,0 +1,22 @@
+-- Projekt beállítása
+set_project("CryptoBot")
+set_version("1.0.0")
+set_xmakever("2.7.3")
+
+-- Fordítási beállítások
+set_languages("cxx17")
+add_rules("mode.debug", "mode.release")
+
+-- Csomagok hozzáadása (automatikus letöltés)
+add_requires("qt6", {system = false})
+add_requires("ixwebsocket", {system = false})
+add_requires("nlohmann_json", {system = false})
+add_requires("libcurl", {system = false})
+
+-- Fő target
+target("CryptoBot")
+    set_kind("binary")
+    add_files("src/*.cpp")
+    add_includedirs("include", {public = true})
+    add_packages("qt6", "ixwebsocket", "nlohmann_json", "libcurl")
+    add_links("Qt6Widgets", "Qt6Network")
